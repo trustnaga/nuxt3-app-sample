2f376538cb5c9bbfce134b4228f2362c6c05e040
trustnaga <bushan.kancharla@gmail.com>
2023-03-16 11:07:29 -0400

    initia

--- plugins/auth.js
+++ plugins/auth.js
@@ -3,8 +3,8 @@ import { PublicClientApplication } from "@azure/msal-browser";
 export default defineNuxtPlugin((nuxtApp) => {
     const msalConfig = {
         auth: {
-            clientId: 'a8212ad6-c26e-408a-b7be-44e7c6de294e',
-            authority: 'https://login.microsoftonline.com/16b3c013-d300-468d-ac64-7eda0820b6d3',
+            clientId: '<YOUR_APPLICAION_CLIENT_ID>',
+            authority: 'https://login.microsoftonline.com/<YOU_APPLICAION_TENANT_ID>',
             redirectUri: '/', // Must be registered as a SPA redirectURI on your app registration
             postLogoutRedirectUri: 'http://localhost:3000/' // Must be registered as a SPA redirectURI on your app registrationregistration
         },
